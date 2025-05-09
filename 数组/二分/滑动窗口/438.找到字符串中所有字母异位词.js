832199d367a4dcc62ee237ca2910ef3c8ff64efb
gaoxing <gaojiaxing@shoplazza.com>
2025-05-09 10:04:39 +0800

    feat: 滑动窗口结束

--- 数组/二分/滑动窗口/438.找到字符串中所有字母异位词.js
+++ 数组/二分/滑动窗口/438.找到字符串中所有字母异位词.js
@@ -3,4 +3,39 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function (s, p) {};
+var findAnagrams = function (s, p) {
+  let need = {};
+  let window = {};
+  for (let item of p) {
+    need[item] = (need[item] || 0) + 1;
+  }
+  let left = 0;
+  let right = 0;
+  let valid = 0;
+  const result = [];
+  while (right < s.length) {
+    let rightItem = s[right];
+    right++;
+    if (need[rightItem]) {
+      window[rightItem] = (window[rightItem] || 0) + 1;
+      if (window[rightItem] === need[rightItem]) {
+        valid++;
+      }
+    }
+    while (valid === Object.keys(need).length) {
+      if (right - left === p.length) {
+        result.push(left);
+      }
+      let leftItem = s[left];
+      left++;
+      if (need[leftItem]) {
+        if (window[leftItem] === need[leftItem]) {
+          valid--;
+        }
+        window[leftItem]--;
+      }
+    }
+  }
+
+  return result;
+};
